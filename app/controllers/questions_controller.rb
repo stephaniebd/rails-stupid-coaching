ef69127e83d7d24ebcd944c96e1f52155b667c6c
Stephanie BeauzileD <stephanie.beauzile.d@gmail.com>
2020-05-11 19:24:22 -0400

    continued to work on styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,3 +16,19 @@ class QuestionsController < ApplicationController
     end
   end
 end
+
+# def answer
+  #   if params[:question] == nil || ""
+  #     @question = 'Nothing...'
+  #     @answer = 'Nothing...'
+  #   else
+  #     @question = params[:question]
+  #     if params[:question] == 'I am going to work'
+  #       @answer = 'Great!'
+  #     elsif params[:question].end_with?('?')
+  #       @answer = 'Silly question, get dressed and go to work!'
+  #     else
+  #       @answer = "I don't care, get dressed and go to work!"
+  #     end
+  #   end
+  # end
